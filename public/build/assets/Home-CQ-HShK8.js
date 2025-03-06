1e08f0f20a811c38c3ce208eb125b5c59a9b010f
Eduardo <eduardofp640@gmail.com>
2025-03-06 00:58:21 +0000

    Instalacion dependencias

---
+++ public/build/assets/Home-CQ-HShK8.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-BM8vArYW.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"¡Hola desde React y Laravel con Inertia!"})});export{t as default};
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+// resources/js/Pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>¡Hola desde React y Laravel con Inertia!</h1>
+        </div>
+    );
+}
+
+export default Home;
